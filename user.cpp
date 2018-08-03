094f8674746b63d7dff2e9e64cacbb4e989328b6
Aryan Kumar <aryan.kumar488@gmail.com>
2018-08-03 16:08:26 +0530

    Add files via upload

---
+++ user.cpp
@@ -0,0 +1,28 @@
+void init_trie()
+{
+	struct trie
+	{
+		int prefix;
+		trie 
+	};
+}
+
+void Tries_insert(const char *key)
+{
+
+}
+
+bool Tries_search(const char *key)
+{
+    return 0;
+}
+
+int Tries_CountofkeysWithPrefix(const char *key)
+{
+    return 0;
+}
+
+void Tries_deleteKey(const char *key)
+{
+
+}
